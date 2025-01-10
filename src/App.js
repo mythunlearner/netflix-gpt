bd15c1a0b8505a734a1a6862b0806a4886b864e6
mythunlearner <learntechpassion@gmail.com>
2025-01-10 12:06:10 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-600">
+      Hi Every one , Let build netFlix GPT !
+    </div>
+  );
+}
+
+export default App;
