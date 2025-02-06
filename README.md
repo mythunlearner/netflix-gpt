08db43f76d7d8c948d1635d1618dc16fb045c8a2
mythunlearner <learntechpassion@gmail.com>
2025-02-06 10:29:30 +0530

    Multi language Support in GPTSearch page

--- README.md
+++ README.md
@@ -105,8 +105,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 - Build Movie card
 - TMDB Image CDN URL
 - usePopularMovies, UseUpcomingMovies
-
-
+- Added MultiLingual for CHAT GPT search
+- GPT Serarch Bar
+- GPT Search Page
 # Features
 - Login/Sign Up
   - Sign In / Sign Up Form
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,13 @@ import { addUser, removeUser } from '../utils/userSlice';
 import { useDispatch } from 'react-redux';
 import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
+  const showGptSearch =useSelector((store) => store.gpt.showGptSearch)
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -46,30 +48,41 @@ const Header = () => {
     //Toggle GPT Search
     dispatch(toggleGptSearchView());
   };
+
+  const handlelanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
-     
-      <img className='w-36' 
-       src={LOGO}
-       alt="logo"/>
-       {user && <div className="flex  p-4">
-        <select className="p-2 m-2 bg-gray-500 text-white rounded-lg">
-          {SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
-           {/* <option value="en">English</option>
-           <option value="tamil">Tamil</option>
-           <option value="hindi">Hindi</option>
-           <option value="spanish">Spanish</option> */}
-        </select>
-        <button className="text-black py-2 px-4 mx-4 my-2  bg-amber-50 rounded-lg align-middle"
-        onClick={handleGptSearchClick}>CHAT GPT</button>
-        <img
-         className="w-12 h-12"
-         alt="usericon"
-         src={user?.photoURL}/>
-         <button onClick={handleSignOut} className="text-white">Sign Out</button>
-       </div>}
+      <img className="w-36" src={LOGO} alt="logo" />
+      {user && (
+        <div className="flex  p-4">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-500 text-white rounded-lg"
+              onChange={handlelanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="text-black py-2 px-4 mx-4 my-2  bg-amber-50 rounded-lg align-middle"
+            onClick={handleGptSearchClick}
+          >
+           {showGptSearch? "Home" : "GPT Search"}
+          </button>
+          <img className="w-12 h-12" alt="usericon" src={user?.photoURL} />
+          <button onClick={handleSignOut} className="text-white">
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Header;
